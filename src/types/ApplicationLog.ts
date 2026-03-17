8b3c9f6435573661430b130605ec0dc4f4916f70
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-03-17 11:57:40 -0300

    feat: adicionar tipos para Insulin e ApplicationLog

---
+++ src/types/ApplicationLog.ts
@@ -0,0 +1,6 @@
+export interface ApplicationLog {
+    id: number;
+    appliedUnits: number;
+    applicationTime: string; // Ex: 2026-03-13T10:30:00
+    consumedCarbs: number;
+}
---
+++ src/types/Insulin.ts
@@ -0,0 +1,12 @@
+export interface Insulin {
+    id: number;
+    name: string;
+    description: string;
+    units: number;
+    price: number;
+    quantity: number;
+    openingDate: string;
+    purchaseDate: string;
+    types: string;
+    format: string;
+}
